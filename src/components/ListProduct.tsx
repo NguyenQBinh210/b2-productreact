6621b7ced7a86612cd69fa854d88c1fe853c60ca
Binh Nguyen <nguyenqbinh210@gmail.com>
2025-03-10 20:37:53 +0700

    fn

--- src/components/ListProduct.tsx
+++ src/components/ListProduct.tsx
@@ -1,15 +1,13 @@
 import { useState, useEffect } from "react";
 import { ProductElement } from "../types/product";
 
-
 const ListProduct = () => {
   const [products, setProducts] = useState<ProductElement[]>([]);
 
   useEffect(() => {
-    fetch("/data.json") // Đảm bảo đặt file data.json trong thư mục public
+    fetch("/data.json")
       .then((res) => res.json())
-      .then((data) => setProducts(data)) // Chắc chắn data là mảng sản phẩm
-      .catch((error) => console.error("Error loading products:", error));
+      .then((data) => setProducts(data));
   }, []);
 
   return (
